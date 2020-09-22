9371288f4d01184d372693bf9f2c29bafe02e5ba
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-09-22 14:03:08 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'assets/css/style.css';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6xl">Javascript React</h1>
+    </div>
+  );
+}
+
+export default App;
